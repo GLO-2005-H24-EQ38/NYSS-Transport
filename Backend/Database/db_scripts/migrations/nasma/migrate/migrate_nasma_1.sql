f563d9b5101cea502e0dfc2fbf44d165ce4c813a
Nasma Chaoui <nasma.chaoui.1@ulaval.ca>
2024-04-05 15:49:23 -0400

    fix: 1) put every return in JSON format (my functions only) 2) added numberOfPassage to every function 3) added insert to ticket or subscription when adding an access

--- Backend/Database/db_scripts/migrations/nasma/migrate/migrate_nasma_1.sql
+++ Backend/Database/db_scripts/migrations/nasma/migrate/migrate_nasma_1.sql
@@ -1,30 +1,44 @@
 DROP FUNCTION IF EXISTS SearchAccess;
 DROP FUNCTION IF EXISTS RetrieveWallet;
-DROP PROCEDURE IF EXISTS AddAccess;
+DROP FUNCTION IF EXISTS AddAccess;
 
 DELIMITER //
 
 CREATE FUNCTION SearchAccess (
     p_company_name VARCHAR(100),
     p_access_name VARCHAR(100),
-    p_access_type FLOAT
+    p_access_type FLOAT,
+    p_numberOfPassage integer
 )
 RETURNS VARCHAR(10000) DETERMINISTIC
 BEGIN
     DECLARE p_access_list VARCHAR(10000);
 
-    -- Initialize the access list
+    -- Initialize the access list as a JSON array
     SET p_access_list = '';
 
     -- Concatenate access details to the access list
     SELECT GROUP_CONCAT(
-        CONCAT('(', a.id, ', "', a.name, '", ', a.price, ', "', a.type, '", ', a.duration, ')', '\n')
-        SEPARATOR ''
+        CONCAT(
+            '{"accessId": "', a.id, '",',
+            '"accessName": "', a.name, '",',
+            '"price": ', a.price, ',',
+            '"accessType": "', a.type, '",',
+            '"duration": ', a.duration, ',',
+            '"company": "', a.company, '"',
+            IF(a.type = 'ticket', CONCAT(',"numberOfPassage": ', t.passes), ''),
+            '}'
+        ) SEPARATOR ','
     ) INTO p_access_list
     FROM access a
+    LEFT JOIN ticket t ON a.id = t.access
     WHERE (p_company_name IS NULL OR a.company = p_company_name)
         AND (p_access_name IS NULL OR a.name = p_access_name)
-        AND (p_access_type IS NULL OR a.type = p_access_type);
+        AND (p_access_type IS NULL OR a.type = p_access_type)
+        AND (p_numberOfPassage IS NULL OR t.passes = p_numberOfPassage);
+
+    -- Finalize the JSON array
+    SET p_access_list = CONCAT('[',p_access_list, ']');
 
     RETURN p_access_list;
 END //
@@ -41,29 +55,72 @@ BEGIN
 
     -- Retrieve access information for the user
     SELECT GROUP_CONCAT(
-        CONCAT('Access: ', a.name, ', Expiration Date: ', t.expirationDate, ', Access Number: ', t.accessNumber, ', Transaction Date: ', t.transactionDate, ', Transaction Number: ', t.transactionNumber, '\n')
-        SEPARATOR ''
+        CONCAT(
+            '{"accessNumber": "', t.accessNumber, '",',
+            '"accessName": "', a.name, '",',
+            '"accessType": "', a.type, '",',
+            '"transactionDate": ', t.transactionDate, ',',
+            '"expirationDate": ', t.expirationDate, ',',
+            '"transactionNumber": "', t.transactionNumber, '"',
+            '"company": "', a.company, '"',
+            IF(a.type = 'ticket', CONCAT(',"numberOfPassage": ', tk.passes), ''),
+            '}'
+        ) SEPARATOR ','
     ) INTO wallet_info
     FROM transaction t
     JOIN access a ON t.accessId = a.id
+    LEFT JOIN ticket tk ON a.id = tk.access
     WHERE t.user = p_email;
+
+    -- Finalize the JSON array
+    SET wallet_info = CONCAT('[', wallet_info, ']');
 
     RETURN wallet_info;
 END //
 
-CREATE PROCEDURE AddAccess(
-    -- TODO Confirmer que l'access_id est donné (généré déjà par l'api)--
-    IN p_access_id integer,
-    IN p_access_name VARCHAR(100),
-    IN p_price FLOAT,
-    IN p_company_name VARCHAR(100),
-    IN p_access_type enum('ticket','subscription'),
-    IN p_duration INT
-)
+CREATE FUNCTION AddAccess(
+    p_access_id integer,
+    p_access_name VARCHAR(100),
+    p_price FLOAT,
+    p_company_name VARCHAR(100),
+    p_access_type enum('ticket','subscription'),
+    p_duration INT,
+    p_numberOfPassage integer
+) RETURNS VARCHAR(10000) DETERMINISTIC
 BEGIN
-    INSERT INTO company (name) VALUES (p_company_name);
+    DECLARE access_created VARCHAR(10000);
+    -- Insertion de la compagnie si elle n'existe pas déjà
+    INSERT IGNORE INTO company (name) VALUES (p_company_name);
+
+    -- Insertion dans la table access
     INSERT INTO access (id, name, price, company, type, duration)
     VALUES (p_access_id, p_access_name, p_price, p_company_name, p_access_type, p_duration);
+
+     -- Insertion dans la table appropriée en fonction du type d'accès
+    IF p_access_type = 'ticket' THEN
+        INSERT INTO ticket (access, passes) VALUES (p_access_id, p_numberOfPassage);
+    ELSEIF p_access_type = 'subscription' THEN
+        INSERT INTO subscription (access) VALUES (p_access_id);
+    END IF;
+
+    SET access_created = '';
+
+    SELECT GROUP_CONCAT(
+        CONCAT(
+            '{"accessId": "', p_access_id, '",',
+            '"accessName": "', p_access_name, '",',
+            '"price": ', p_price, ',',
+            '"accessType": "', p_access_type, '",',
+            '"duration": ', p_duration, ',',
+            '"company": "', p_company_name, '"',
+            IF(p_access_type = 'ticket', CONCAT(',"numberOfPassage": ', p_numberOfPassage), ''),
+            '}'
+        ) SEPARATOR ','
+    ) INTO access_created;
+
+    SET access_created = CONCAT('[',access_created, ']');
+
+    RETURN access_created;
 END //
 DELIMITER ;
 
@@ -109,8 +166,8 @@ DELIMITER ;
  VALUES (1, 5);
 
  -- Inserting sample data into the subscription table
- INSERT INTO subscription (access, start)
- VALUES (2, '2024-01-01');
+ INSERT INTO subscription (access)
+ VALUES (2), (4), (5);
 
  -- Inserting sample data into the transaction table
  INSERT INTO transaction (accessNumber, transactionNumber, creditCard, user, accessId, transactionDate, expirationDate)
