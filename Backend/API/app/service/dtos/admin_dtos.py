fb4b79e76d6985e88daaf6f1746749316cf89df2
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2024-04-12 15:21:42 -0400

    fix: fix Login generates same token for all users (commuter and admin)
    now generate sa new one everytime

--- Backend/API/app/service/dtos/admin_dtos.py
+++ Backend/API/app/service/dtos/admin_dtos.py
@@ -10,8 +10,8 @@ from app.service.exceptions import *
 class Token():
     """Used for user authentication"""
 
-    def __init__(self, token=uuid.uuid4().hex):
-        self.value = token
+    def __init__(self, token=None):
+        self.value = uuid.uuid4().hex if token is None else token
 
     def __str__(self):
         return self.value
