78cc0f18402783da29c1224e6c6f0655ff05ec78
Ahmed Sami <ahmed.sami.1@ulaval.ca>
2024-04-13 19:23:54 -0400

    refact: add search UI component

--- Frontend/src/views/HomeView/HomeView.vue
+++ Frontend/src/views/HomeView/HomeView.vue
@@ -1,10 +1,11 @@
 <script>
 import AccessContainer from "@/components/Access/Communter/AccessContainer.vue";
 import Cookies from "js-cookie";
+import SearchSideBar from "@/views/HomeView/SearchSideBar.vue";
 
 export default {
   name: "HomeView",
-  components: {AccessContainer},
+  components: {SearchSideBar, AccessContainer},
   data() {
     return {
 
@@ -14,10 +15,27 @@ export default {
     if (!Cookies.get('commuterToken')) {
       this.$router.push('/login');
     }
+    document.querySelector('.access-container').style.overflow = 'auto';
+    document.body.style.overflow = 'hidden';
   },
 }
 </script>
 
 <template>
-  <AccessContainer />
+  <div style="display: flex; flex-direction: row">
+    <div id="justBeforeBar" style="flex:1; display: flex; justify-content: center;   background-color: #f3f3f3;">
+            <SearchSideBar />
+    </div>
+    <div class="access-container" style="flex:5; overflow: auto;">
+          <AccessContainer/>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.access-container {
+  height: 100vh;
+  overflow-y: auto;
+}
+
+</style>
---
+++ Frontend/src/views/HomeView/SearchSideBar.vue
@@ -0,0 +1,70 @@
+<script>
+export default {
+  name: "SearchSideBar"
+}
+</script>
+
+<template>
+    <div class="search-bar" style="display: flex; justify-content: center; align-items: center">
+      <div class="search-container">
+        <div style="font-size: 1.5rem; margin-bottom: 2rem;"><strong>Looking for a specific access ?</strong></div>
+        <div>
+          <div style="margin-left: 0.5rem">Search By Company</div>
+          <input type="text" placeholder="RTC" class="search-input">
+        </div>
+        <div>
+          <div style="margin-left: 0.5rem">Search By Price</div>
+          <input type="number" placeholder="5.00" class="search-input">
+        </div>
+        <div>
+          <div style="margin-left: 0.5rem">Search By Access Name</div>
+          <input type="text" placeholder="Access Weekend" class="search-input">
+        </div>
+        <div>
+          <div style="margin-left: 0.5rem">Search By Access Type</div>
+          <select>
+            <option value="Ticket">Ticket</option>
+            <option value="Subscription">Subscription</option>
+          </select>
+        </div>
+        <div style="width: 100%; display: flex; justify-content: center">
+          <button class="search-button">Search</button>
+        </div>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+input {
+  padding: 0.5rem;
+  margin: 0.5rem;
+  border-radius: 1rem;
+  border: none;
+}
+
+select {
+  padding: 0.5rem;
+  margin: 0.5rem;
+  border-radius: 1rem;
+  border: none;
+}
+
+button {
+  width: 100%;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  border-radius: 1rem;
+  border: none;
+  background-color: #01356a;
+  color: white;
+}
+
+.search-container {
+  padding: 1rem;
+  border-radius: 0.5rem;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 1rem;
+}
+</style>
